e939a53fca9f11ee253fa508c48ed69f78bbdb18
rvers <saurabhsuryan99@hotmail.com>
2021-09-20 14:59:11 +0530

    ✨ removes /build

--- build/dist/components/GridOptions.js
+++
@@ -1,8 +0,0 @@
-import React from "../../_snowpack/pkg/react.js";
-import styles from "../styles/GridOptions.module.css.proxy.js";
-const GridOptions = () => {
-  return /* @__PURE__ */ React.createElement("div", {
-    className: styles.container
-  }, /* @__PURE__ */ React.createElement("button", null, "View"), /* @__PURE__ */ React.createElement("button", null, "Delete"));
-};
-export default GridOptions;
--- build/dist/index.js
+++
@@ -1,5 +0,0 @@
-import React from "../_snowpack/pkg/react.js";
-import ReactDOM from "../_snowpack/pkg/react-dom.js";
-import App from "./components/App.js";
-import "./styles/index.css.proxy.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("root"));
